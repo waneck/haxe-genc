507b55dc3dae5fe40f44852da5ca6b78cbee1f94
Simon Krajewski <simon@haxe.org>
2013-06-28 22:32:18 +0200

    fix Pointer operators

--- std/c/Pointer.hx
+++ std/c/Pointer.hx
@@ -21,10 +21,26 @@
  */
 package c;
 
-@:notNull @:runtimeValue abstract Pointer<T>(T)
-{
-	@:extern @:op(A+B) public static function add<T>(addr:Pointer<T>, offset:Int):Pointer<T> { return addr;}
-	@:extern @:op(++A) public static function increment<T>(addr:Pointer<T>):Pointer<T> { return addr; }
-	@:extern @:arrayAccess public inline function __get(index:Int):T { return untyped this[index]; }
-	@:extern @:arrayAccess public inline function __set(index:Int, value:T):T { return untyped this[index] = value; }
+@:notNull
+@:runtimeValue
+abstract Pointer<T>(Int) {
+	public function new(i:Int) {
+		this = i;
+	}
+	
+	@:extern @:op(A+B) public function add(offset:Int):Pointer<T> {
+		return new Pointer<T>(this + offset);
+	}
+	
+	@:extern @:op(++A) public inline function increment<T>():Pointer<T> {
+		return new Pointer(++this);
+	}
+	
+	@:extern @:arrayAccess public inline function __get(index:Int):T {
+		return untyped this[index];
+	}
+	
+	@:extern @:arrayAccess public inline function __set(index:Int, value:T):T {
+		return untyped this[index] = value;
+	}
 }
