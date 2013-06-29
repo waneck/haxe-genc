8c73fb4541e27829a55dc59a3f8ec99560b33c67
Simon Krajewski <simon@haxe.org>
2013-06-29 12:05:25 +0200

    add ctype extern

---
+++ std/c/std/Ctype.hx
@@ -0,0 +1,21 @@
+package c.std;
+
+import c.Types;
+import c.Pointer;
+
+@:include("<ctype.h>")
+extern class Ctype {
+	@:plain static public function isalnum(ch:Int):Int;
+	@:plain static public function isalpha(ch:Int):Int;
+	@:plain static public function iscntrl(ch:Int):Int;
+	@:plain static public function isdigit(ch:Int):Int;
+	@:plain static public function isgraph(ch:Int):Int;
+	@:plain static public function islower(ch:Int):Int;
+	@:plain static public function isprint(ch:Int):Int;
+	@:plain static public function ispunct(ch:Int):Int;
+	@:plain static public function isspace(ch:Int):Int;
+	@:plain static public function isupper(ch:Int):Int;
+	@:plain static public function isxdigit(ch:Int):Int;
+	@:plain static public function tolower(ch:Int):Int;
+	@:plain static public function toupper(ch:Int):Int;
+}
