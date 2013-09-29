54a80151587668d813b894b888f5c82efa4a43f4
Simon Krajewski <simon@haxe.org>
2013-09-29 13:47:52 +0200

    avoid optimizer

--- tests/c/src/ctest/If.unit.hx
+++ tests/c/src/ctest/If.unit.hx
@@ -1,21 +1,23 @@
 var x;
-if (2 > 1) {
+var two = 2;
+var one = 1;
+if (two > one) {
 	x = 1;
 } else {
 	x = 2;
 }
 x == 1;
 
-if (1 > 2) {
+if (one > two) {
 	x = 1;
 } else {
 	x = 2;
 }
 x == 2;
 
-if (1 > 2) {
+if (one > two) {
 	x = 1;
-} else if (2 > 1) {
+} else if (two > one) {
 	x = 2;
 } else {
 	x = 3;
