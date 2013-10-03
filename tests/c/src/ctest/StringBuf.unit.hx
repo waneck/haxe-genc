819607dfc651591a3f08de2652ffd0cb96e42f2d
Simon Krajewski <simon@haxe.org>
2013-10-03 02:12:40 +0200

    add some compiling unit tests from main haxe

--- tests/c/src/ctest/StringBuf.unit.hx
+++ tests/c/src/ctest/StringBuf.unit.hx
@@ -8,3 +8,31 @@ b.addChar('z'.code);
 b.toString() == "foobarz";
 b.addSub("babaz", 2);
 b.toString() == "foobarzbaz";
+
+// add, toString
+var x = new StringBuf();
+x.toString() == "";
+x.add(null);
+x.toString() == "null";
+
+// addChar
+var x = new StringBuf();
+x.addChar(32);
+x.toString() == " ";
+
+// addSub
+var x = new StringBuf();
+x.addSub("abcdefg", 1);
+x.toString() == "bcdefg";
+var x = new StringBuf();
+x.addSub("abcdefg", 1, null);
+x.toString() == "bcdefg";
+var x = new StringBuf();
+x.addSub("abcdefg", 1, 3);
+x.toString() == "bcd";
+
+// identity
+function identityTest(s:StringBuf) {
+	return s;
+}
+identityTest(x) == x;
