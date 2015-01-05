c275ee351c42d0cc1e508715b7e1f2d7a30bded7
Johann Borck <ousado@tormail.org>
2015-01-05 17:36:59 +0000

    std lib changes

---
+++ structures.ml
@@ -0,0 +1,31 @@
+
+
+(*
+function (v:bool) 0{
+	if (v) 0.0{
+		a
+	}0.0 else 0.1{
+		b
+	}0.1
+}0
+*)
+
+
+static_scope
+instance_scope
+function_scope
+closure_scope
+branch_scope
+loop_scope
+block_scope
+
+
+scope 0 [scope 0.0; scope 0.1]
+
+X.a.b = y
+X.cs[0].cs[1] = v;
+
+effects on value at static path   P
+effects on value at instance path P
+effects on value at local path    P
+
